babc0f834e841c36b2eda61bd30ead91b7e3aa27
Bill.Dagou <billdagou@gmail.com>
2022-11-22 08:32:57 +0800

    Init

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['ckeditor5'] = [
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['ckeditor'] = [
     'Dagou\\Ckeditor5\\ViewHelpers',
 ];
